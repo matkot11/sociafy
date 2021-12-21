e97b42f7947268853993f69824d3145a4a9bfe34
Mateusz Kocik <matkot11@gmail.com>
2021-12-21 23:18:23 +0100

    Added animation to the loader

--- components/organisms/Loading/Loading.js
+++ components/organisms/Loading/Loading.js
@@ -1,30 +1,74 @@
+import { useEffect, useRef } from "react";
 import Image from "next/image";
 import { Wrapper } from "./Loading.styles";
+import gsap from "gsap";
 
-const Loading = () => (
-  <Wrapper>
-    <Image
-      src={"/svg/loadingMessage.svg"}
-      alt="Message svg"
-      layout="fixed"
-      width={300}
-      height={100}
-    />
-    <Image
-      src={"/svg/loadingMessage.svg"}
-      alt="Message svg"
-      layout="fixed"
-      width={300}
-      height={100}
-    />
-    <Image
-      src={"/svg/loadingMessage.svg"}
-      alt="Message svg"
-      layout="fixed"
-      width={300}
-      height={100}
-    />
-  </Wrapper>
-);
+const Loading = () => {
+  const image = useRef(null);
+
+  useEffect(() => {
+    const { current: e } = image;
+    const message = e.querySelectorAll("span");
+    const tl = gsap.timeline({
+      repeat: -1,
+      duration: 1.2,
+    });
+    tl.fromTo(
+      message[0],
+      {
+        opacity: 1,
+        y: 0,
+        ease: "easeInCirc",
+      },
+      {
+        opacity: 0,
+        y: 50,
+      },
+      "0",
+    )
+      .fromTo(
+        message[1],
+        {
+          opacity: 1,
+          y: 0,
+          ease: "easeInCirc",
+        },
+        {
+          opacity: 0,
+          y: 50,
+        },
+        "+0.3",
+      )
+      .fromTo(
+        message[2],
+        {
+          opacity: 1,
+          y: 0,
+          ease: "easeInCirc",
+        },
+        {
+          opacity: 0,
+          y: 50,
+        },
+        "+0.5",
+      );
+  }, []);
+
+  return (
+    <Wrapper ref={image}>
+      {[0, 1, 2].map((i) => (
+        <Image
+          key={i}
+          src={"/svg/loadingMessage.svg"}
+          alt={`${i + 1} loader`}
+          layout="fixed"
+          width={300}
+          height={100}
+          priority="true"
+        />
+      ))}
+    </Wrapper>
+  );
+};
 
 export default Loading;
--- components/organisms/Loading/Loading.styles.js
+++ components/organisms/Loading/Loading.styles.js
@@ -1,28 +1,4 @@
-import styled, { keyframes } from "styled-components";
-
-const loadingAnimation = keyframes`
-  from {
-    opacity: 1;
-    transform: translateY(0);
-  }
-
-  to {
-    opacity: 0;
-    transform: translateY(4rem);
-  }
-`;
-
-const loadingAnimationSecondChild = keyframes`
-  from {
-    opacity: 1;
-    transform: translateY(0) rotateY(180deg);
-  }
-
-  to {
-    opacity: 0;
-    transform: translateY(4rem) rotateY(180deg);
-  }
-`;
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
   max-width: 100vw;
@@ -36,15 +12,7 @@ export const Wrapper = styled.div`
     margin: 3rem !important;
   }
 
-  span:nth-child(1) {
-    animation: ${loadingAnimation} 1.5s ease-in infinite;
-  }
-
   span:nth-child(2) {
-    animation: ${loadingAnimationSecondChild} 1.5s ease-in infinite;
-  }
-
-  span:nth-child(3) {
-    animation: ${loadingAnimation} 1.5s ease-in infinite;
+    transform: rotateY(180deg);
   }
 `;
