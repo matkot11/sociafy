16a2ae591cbcb2e5b5a14551d95a722d51e27e95
Mateusz Kocik <matkot11@gmail.com>
2021-12-24 12:07:01 +0100

    Added .env.local file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    mongoDBAPI:
-      "mongodb+srv://matkot11:1YIlCrQyz2WmoxN2@cluster0.bf2g6.mongodb.net/auth?retryWrites=true&w=majority",
-  },
 };
